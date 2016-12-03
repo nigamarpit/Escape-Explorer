92bd40d7a866cfc204add092ec6b1721c1fa81ee
Arpit Nigam <arpit.nigam@outlook.com>
2016-12-03 15:47:32 -0800

    Add project files.

---
+++ IIW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IIW.Startup))]
+namespace IIW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
